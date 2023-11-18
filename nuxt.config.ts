e461664f001c4a6397c27fe78e632a0f3835cfbc
ARIE\Lenovo <kashikiku.kyungrie12@gmail.com>
2023-11-18 17:10:34 +0700

    update port to 3001

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  
-  vite: {
-    server: {
-        fs: {
-            allow: ["/home/user/Monorepo"]
-        }
-    }
-}
 })
